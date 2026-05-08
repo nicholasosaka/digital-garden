b9e383c38a49e4dff04ccadd81c858c46e50b56e
Nicholas Osaka <nicholas.osaka@gmail.com>
2026-05-07 23:58:03 -0400

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -46,8 +46,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
+Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
